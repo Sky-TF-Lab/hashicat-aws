b782d870bfd002a8db3159938f9f1ce84386e213
Sky-TF-Lab <skytflab@skyfire.click>
2023-03-27 03:35:32 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,7 @@
 module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
+  source  = "app.terraform.io/Sky-TF-Lab/s3-bucket/aws"
+  version = "2.8.0"
+}
   bucket = "${var.prefix}-bkt1"
   acl    = "private"
 
